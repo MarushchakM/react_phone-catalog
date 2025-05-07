c01d0f1a0b27eec3409c9c62bfba4bd986b57068
marushchak <marushchak099@gmail.com>
2025-05-07 20:24:37 +0300

    test

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { App } from './App';
 import { HomePage } from './pages/HomePage';
